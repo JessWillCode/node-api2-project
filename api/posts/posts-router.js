b4a9e7bad108255f47c14ca6b1149dc586ba12bb
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-08 17:50:29 -0500

    completed through post endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,120 @@
-// implement your posts router here
+const router = require('express').Router()
+
+const Post = require('./posts-model');
+
+// //POSTS ENDPOINTS
+router.get('/', (req,res) => {
+    Post.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'The posts information could not be retrieved'
+        })
+    })
+})
+
+router.get('/:id', (req,res) => {
+    Post.findById(req.params.id)
+    .then(post => {
+        if (!post) {
+            res.status(404).json({
+            message: 'The post with the specified ID does not exist'
+        })
+        } else {
+            res.status(200).json(post);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'The post information could not be retrieved'
+        })
+    })
+})
+
+router.post('/', (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    } else {
+        Post.insert(req.body)
+        .then(post => {
+            res.status(201).json(post)
+        }) 
+        .catch(err => {
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database'
+            })
+        })
+    }
+})
+
+// router.put('/:id', (req,res) => {
+//    if(!req.params.id) {
+//     res.status(404).json({
+//         message: 'The post with the specified ID does not exist'
+//     })
+// } else {
+//     if(!req.body.title || !req.body.contents) {
+//     res.status(400).json({
+//         message: 'Please provide title and contents for the post'
+//         })
+//     } else {
+//     Post.update(req.params.id, req.body)
+//         .then(post => {
+//             res.status(201).json(post)
+//         })
+//         .catch(err => {
+//         res.status(500).json({
+//             message: 'The post information could not be modified'
+//         })
+//     })
+//     }
+// }
+// })
+
+// router.delete('/:id', (req,res) => {
+//     Post.remove()
+//     .then(deletedPost => {
+//         if(!deletedPost) {
+//             res.status(404).json({
+//                 message: 'The post with the specified ID does not exist'
+//             })
+//         } else {
+//             res.status(200).json({
+//                 message: 'The post has been nuked'
+//             })
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json({
+//             message: 'The post could not be removed'
+//         })
+//     })
+
+
+// })
+
+// router.get('/:id/comments', (req,res) => {
+//     Post.findPostComments()
+//     .then(post => {
+//         if(!post) {
+//             res.status(404).json({
+//                 message: 'The post with the specified ID does not exist'
+//             })
+//         } else {
+//             res.status(200).json(post.comments)
+//         }
+//     })
+//     .catch(err => {
+//         res.staus(500).json({
+//             message: 'The comments information could not be retrieved'
+//         })
+//     })
+// })
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// require your server and launch it here
+const server = require('./api/server.js')
+
+server.listen(9000, () => {
+    console.log('magic happening on server 9000')
+})
