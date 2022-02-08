8823dd0d31e74724c0e679fc078581325e89ef87
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-08 18:59:53 -0500

    completed mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -111,23 +111,22 @@ router.delete('/:id', async (req,res) => {
 })
 
 
-
-// router.get('/:id/comments', (req,res) => {
-//     Post.findPostComments()
-//     .then(post => {
-//         if(!post) {
-//             res.status(404).json({
-//                 message: 'The post with the specified ID does not exist'
-//             })
-//         } else {
-//             res.status(200).json(post.comments)
-//         }
-//     })
-//     .catch(err => {
-//         res.staus(500).json({
-//             message: 'The comments information could not be retrieved'
-//         })
-//     })
-// })
+router.get('/:id/comments', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            const comments = await Post.findPostComments(req.params.id)
+            res.json(comments)
+        }
+    } catch (err) {
+        res.staus(500).json({
+            message: 'The comments information could not be retrieved'
+})
+    }
+})
 
 module.exports = router;
